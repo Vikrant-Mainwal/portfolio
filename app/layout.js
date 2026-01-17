49b5ecf1f08760cf348da28838a33fbc0ff7de49
Vikrant Mainwal <vikrantmainwal1301@gmail.com>
2026-01-17 12:21:28 +0530

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" class="dark">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
